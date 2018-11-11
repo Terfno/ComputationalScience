a4f76564c555c813ee3f02c92221b55fee4d6d65
terfno <sueda.takahito@gmail.com>
2018-11-11 16:42:20 +0900

    add md html cpp file && fix taniyu file

---
+++ report_terfno15/runge-kutta.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+double f(double x, double y){
+    return (x + y);
+}
+
+int main(){
+    int i;
+    double x, y, h, k1, k2, k3, k4, k;
+    char zz;
+
+    printf("4次のルンゲクッタ法により dy/dx=x+y を解きます。\n\n");
+    printf("\n\nReturnキーを押してください。\n");
+    scanf("%c",&zz);
+
+    printf(" z y\n");
+
+    x = 0.0;
+    y = 1.0;
+    h = 0.1; //刻み幅は0.1
+
+    printf("%10.6lf %10.lf\n",x,y);
+
+    for(i=1;i<=40;i++){
+        k1 = h * f(x, y);
+        k2 = h * f(x + (h / 2), y + (k1 / 2));
+        k3 = h * f(x + (h / 2) , y + (k2 / 2));
+        k4 = h * f(x + h, y + k3);
+        k = (k1 + (2 * k2) + (2 * k3) + k4) / 6;
+        x = x + h;
+        y = y + k;
+        
+        printf("%10.6lf %10.lf\n",x,y);
+    }
+    return 0;
+}
