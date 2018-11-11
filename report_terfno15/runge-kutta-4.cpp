d6e31c4951063ac7e58143b01c19937a930f1c4d
terfno <sueda.takahito@gmail.com>
2018-11-11 17:21:00 +0900

    fix cpp

--- report_terfno15/runge-kutta-4.cpp
+++ report_terfno15/runge-kutta-4.cpp
@@ -11,17 +11,17 @@ int main(){
     double x, y, h, k1, k2, k3, k4, k;
     char zz;
 
-    printf("\n\n4次のルンゲクッタ法により dy/dx=x+y を解きます。\n\n");
+    printf("\n\n4次のルンゲクッタ法により dy/dx=e^{-sin x}-ycos x を解きます。\n\n");
     printf("Returnキーを押してください。\n");
     scanf("%c",&zz);
 
-    printf("  x                 y\n");
+    printf("  x          y\n");
 
     x = 0.0;
     y = 1.0;
     h = 0.1; //刻み幅は0.1
 
-    printf("%10.6lf %10.lf\n",x,y);
+    printf("%10.6lf %10.6lf\n",x,y);
 
     for(i=1;i<=40;i++){
         k1 = h * f(x, y);
@@ -32,7 +32,7 @@ int main(){
         x = x + h;
         y = y + k;
         
-        printf("%10.6lf %10.lf\n",x,y);
+        printf("%10.6lf %10.6lf\n",x,y);
     }
     return 0;
 }
